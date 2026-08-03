c6f20b97e0b4feeadf90c6e538eb604781ee86b3
Lathikasri <lathikasri.a.2025.aiml@rajalakshmi.edu.in>
2026-08-03 22:09:25 +0530

    Add hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+print("Hello , Github!")
+name = "Lathikasri"
+print(f"welcome to my python fundamentals repository!")
+print(f"My name is: {name}.")
+print("This repository documents my python learning journey.")
