97eba829f1fbc4ce735a264b1d5f8dc665a6cff5
Lathikasri <lathikasri.a.2025.aiml@rajalakshmi.edu.in>
2026-08-03 22:18:05 +0530

    Add operators.py

---
+++ operators.py
@@ -0,0 +1,12 @@
+a = 20
+b = 5
+
+print("Arithmetic Operators")
+print("---------------------")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
