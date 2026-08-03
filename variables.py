c29dced09dbe03bef6f7a28e3f789d65c277e195
Lathikasri <lathikasri.a.2025.aiml@rajalakshmi.edu.in>
2026-08-03 22:16:38 +0530

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+name = "Lathikasri"
+age = 18
+college = "Rajalakshmi Engineering College"
+course = "Artificial Intelligence and Machine Learning"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Course:", course)
